0c29a4e922d700aceef79c0ad3e5f0cb1a272381
Wilhelm <wilhelm.thieme@gmail.com>
2017-07-16 16:20:18 +0200

    Start and finish buttons replaced by ready checkboxes Scoreactivity integrated with mainactivity (Work in progress)

--- tsg_app/src/main/java/nl/whitedove/thespygame/MainActivity.java
+++ tsg_app/src/main/java/nl/whitedove/thespygame/MainActivity.java
@@ -636,8 +636,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        CheckBox cbReady = (CheckBox) findViewById(R.id.cbReady);
-        InitReadyCheckbox(cbReady, false);
+        Switch swReady = (Switch) findViewById(R.id.swReady);
+        InitReadySwitch(swReady, false);
         Switch swReadyScore = (Switch) findViewById(R.id.swReadyScore);
         InitReadySwitch(swReadyScore, false);
 
@@ -1275,10 +1275,16 @@ public class MainActivity extends AppCompatActivity {
         TextView tvLocation = (TextView) findViewById(R.id.tvLocation);
         TextView tvTimer = (TextView) findViewById(R.id.tvTimer);
         TextView tvGameStatus = (TextView) findViewById(R.id.tvGameStatus);
-        CheckBox cbReady = (CheckBox) findViewById(R.id.cbReady);
+        Switch swReady = (Switch) findViewById(R.id.swReady);
         Switch swReadyScore = (Switch) findViewById(R.id.swReadyScore);
+        TextView tvReady = (TextView) findViewById(R.id.tvReady);
+        TextView tvReadyPercent = (TextView) findViewById(R.id.tvReadyPercent);
+        TextView tvReadyPercentScore = (TextView) findViewById(R.id.tvReadyPercentScore);
         LinearLayout llButtonRow2 = (LinearLayout) findViewById(R.id.llButtonRow2);
         LinearLayout llButtonRow3 = (LinearLayout) findViewById(R.id.llButtonRow3);
+        LinearLayout llReady = (LinearLayout) findViewById(R.id.llReady);
+        LinearLayout llReadyScore = (LinearLayout) findViewById(R.id.llReadyScore);
+
         RelativeLayout rlMain = (RelativeLayout) findViewById(R.id.rlMain);
         RelativeLayout rlScore = (RelativeLayout) findViewById(R.id.rlScore);
         ListView lvMessages = (ListView) findViewById(R.id.lvMessages);
@@ -1290,6 +1296,8 @@ public class MainActivity extends AppCompatActivity {
         Player player = Helper.GetPlayer(game.getPlayers(), Helper.GetGuid(this), this);
 
         int nrOfPlayers = (game.getPlayers() == null) ? 0 : game.getPlayers().size();
+        int colRed = ContextCompat.getColor(this, R.color.colorRedBackground);
+        int colGreen = ContextCompat.getColor(this, R.color.colorGreenBackground);
 
         String packname = this.getPackageName();
         Resources res = this.getResources();
@@ -1322,7 +1330,7 @@ public class MainActivity extends AppCompatActivity {
                 StopTimerScore();
                 rlMain.setVisibility(View.VISIBLE);
                 rlScore.setVisibility(View.GONE);
-                cbReady.setVisibility(View.GONE);
+                llReady.setVisibility(View.GONE);
                 tvSpy.setVisibility(View.GONE);
                 tvLocation.setVisibility(View.GONE);
                 llButtonRow2.setVisibility(View.VISIBLE);
@@ -1334,9 +1342,11 @@ public class MainActivity extends AppCompatActivity {
                 StopTimerScore();
                 rlMain.setVisibility(View.VISIBLE);
                 rlScore.setVisibility(View.GONE);
-                cbReady.setVisibility(View.VISIBLE);
-                InitReadyCheckbox(cbReady, player.getIsReady());
-                cbReady.setText(String.format(getString(R.string.ReadyToStart), Integer.toString(aantalReady), Integer.toString(nrOfPlayers)));
+                llReady.setVisibility(View.VISIBLE);
+                InitReadySwitch(swReady, player.getIsReady());
+                tvReady.setText(getString(R.string.ReadyToStart));
+                tvReadyPercent.setText(String.format(getString(R.string.ReadyPercent), Integer.toString(aantalReady), Integer.toString(nrOfPlayers)));
+                swReady.setBackgroundColor(player.getIsReady() ? colGreen : colRed);
                 tvSpy.setVisibility(View.GONE);
                 tvLocation.setVisibility(View.GONE);
                 llButtonRow2.setVisibility(View.VISIBLE);
@@ -1348,9 +1358,11 @@ public class MainActivity extends AppCompatActivity {
                 StopTimerScore();
                 rlMain.setVisibility(View.VISIBLE);
                 rlScore.setVisibility(View.GONE);
-                cbReady.setVisibility(View.VISIBLE);
-                InitReadyCheckbox(cbReady, player.getIsReady());
-                cbReady.setText(String.format(getString(R.string.ReadyToFinish), Integer.toString(aantalReady), Integer.toString(nrOfPlayers)));
+                llReady.setVisibility(View.VISIBLE);
+                InitReadySwitch(swReady, player.getIsReady());
+                tvReady.setText(getString(R.string.ReadyToFinish));
+                tvReadyPercent.setText(String.format(getString(R.string.ReadyPercent), Integer.toString(aantalReady), Integer.toString(nrOfPlayers)));
+                swReady.setBackgroundColor(player.getIsReady() ? colGreen : colRed);
                 tvSpy.setVisibility(View.VISIBLE);
 
                 if (player.getIsSpy()) {
@@ -1375,7 +1387,7 @@ public class MainActivity extends AppCompatActivity {
                 rlScore.setVisibility(View.VISIBLE);
                 tvTimer.setText(getString(R.string.TimeZero));
                 tvTimer.setTextColor(Color.RED);
-                swReadyScore.setVisibility(View.GONE);
+                llReadyScore.setVisibility(View.GONE);
                 tvSpy.setVisibility(View.GONE);
                 tvLocation.setVisibility(View.GONE);
                 tvResultWhere.setVisibility(View.GONE);
@@ -1400,11 +1412,9 @@ public class MainActivity extends AppCompatActivity {
                 CastTimeColor(Color.RED);
                 tvTimer.setText(getString(R.string.TimeZero));
                 tvTimer.setTextColor(Color.RED);
-                swReadyScore.setVisibility(View.VISIBLE);
+                llReadyScore.setVisibility(View.VISIBLE);
                 InitReadySwitch(swReadyScore, player.getIsReady());
-                //swReadyScore.setText(String.format(getString(R.string.ReadyToStartNew), Integer.toString(aantalReady), Integer.toString(nrOfPlayers)));
-                int colRed = ContextCompat.getColor(this, R.color.colorRedBackground);
-                int colGreen = ContextCompat.getColor(this, R.color.colorGreenBackground);
+                tvReadyPercentScore.setText(String.format(getString(R.string.ReadyPercent), Integer.toString(aantalReady), Integer.toString(nrOfPlayers)));
                 swReadyScore.setBackgroundColor(player.getIsReady() ? colGreen : colRed);
                 tvSpy.setVisibility(View.VISIBLE);
                 tvLocation.setVisibility(View.VISIBLE);
